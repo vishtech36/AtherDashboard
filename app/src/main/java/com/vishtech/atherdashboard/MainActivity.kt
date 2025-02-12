e0035d6b85878e40344e9186cf607646a3b79d61
VBARVE <vbarve@visteon.com>
2025-02-12 22:20:07 +0530

    Updated PreviewDashboardUI

--- app/src/main/java/com/vishtech/atherdashboard/MainActivity.kt
+++ app/src/main/java/com/vishtech/atherdashboard/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.vishtech.atherdashboard.ui.DashboardUI
 
 class MainActivity : ComponentActivity() {
@@ -14,4 +16,13 @@ class MainActivity : ComponentActivity() {
             DashboardUI()
         }
     }
+}
+
+@Preview(
+    showSystemUi = true,
+    device = "spec:width=411dp,height=891dp,dpi=420,isRound=false,chinSize=0dp,orientation=landscape"
+)
+@Composable
+private fun PreviewDashboardUI() {
+    DashboardUI()
 }
